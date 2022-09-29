eb62760cecfaf76d9ba262c6a7ad2900a274122d
gyulaidavid <gyulaidavid@gmail.com>
2022-09-29 09:13:22 +0200

    f

--- frontend/script.js
+++ frontend/script.js
@@ -45,26 +45,37 @@
 //azok a számok, amikben van 1-es
 
 
-    for (let i = 1; i<=100; i+=1){
-        i.toString();
-        console.log(i.toString());
-  }
+//     for (let i = 1; i<=100; i+=1){
+//         i.toString();
+//         console.log(i.toString());
+//   }
 
-//   let valami = 24;
-//   let hund = valami.toString();
-//   console.log(valami)
-//   console.log(hund)
+// //   let valami = 24;
+// //   let hund = valami.toString();
+// //   console.log(valami)
+// //   console.log(hund)
 
 
-//azok a számok, amikben van 1-es VÉGE
+// //azok a számok, amikben van 1-es VÉGE
 
-//Minden második betűből új string készül
- function stringValue (str, n){
-    let result = ""
-   for (let i = 1; i<str.lenght; i+=2){ 
-        if (str) {
+// //Minden második betűből új string készül
+//  function stringValue (str, n){
+//     let result = ""
+//    for (let i = 1; i<str.lenght; i+=2){ 
+//         if (str) {
 
-        }
-    }
+//         }
+//     }
 
- }
+//  }
+
+let szamok = [2, 4, 13, 4, 6, 42]
+
+let paratlanSzamok = 0;
+for(i=0; i<szamok.length; i++){
+  paratlanSzamok += szamok[i] % 2 === 1 ? 1 : 0
+}
+
+// for (let szam of szamok) {
+
+// }
